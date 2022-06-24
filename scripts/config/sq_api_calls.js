fc9c7bb9d80a0af0288092362cbbb96e2b43ef30
Deepak Balakrishna <deepakbalakrishna@Deepaks-MacBook-Pro.local>
2022-06-24 11:07:14 -0700

    Updated use of constants

--- scripts/config/sq_api_calls.js
+++ scripts/config/sq_api_calls.js
@@ -2,11 +2,17 @@ const axios = require("axios");
 const keys = require("./keys");
 
 // Squadcast API endpoints
-const GET_ACCESS_TOKEN = "https://auth.squadcast.com/oauth/access-token";
-const GET_ALL_TEAMS = "https://api.squadcast.com/v3/teams";
-const GET_ALL_USERS = "https://api.squadcast.com/v3/users";
-const GET_ALL_SCHEDULES = "https://api.squadcast.com/v3/schedules";
-const GET_ALL_ONCALL_EVENTS = "https://api.squadcast.com/v3/schedules";
+// const GET_ACCESS_TOKEN = "https://auth.squadcast.com/oauth/access-token";
+// const GET_ALL_TEAMS = "https://api.squadcast.com/v3/teams";
+// const GET_ALL_USERS = "https://api.squadcast.com/v3/users";
+// const GET_ALL_SCHEDULES = "https://api.squadcast.com/v3/schedules";
+// const GET_ALL_ONCALL_EVENTS = "https://api.squadcast.com/v3/schedules";
+
+const GET_ACCESS_TOKEN = keys.squadcast.GET_ACCESS_TOKEN;
+const GET_ALL_TEAMS = keys.squadcast.GET_ALL_TEAMS;
+const GET_ALL_USERS = keys.squadcast.GET_ALL_USERS;
+const GET_ALL_SCHEDULES = keys.squadcast.GET_ALL_SCHEDULES;
+const GET_ALL_ONCALL_EVENTS = keys.squadcast.GET_ALL_ONCALL_EVENTS;
 
 // Get Access Token - get access token from an API_KEY
 exports.getAccessToken = async (API_KEY) => {
@@ -75,11 +81,12 @@ exports.getAllSchedules = async (access_token) => {
 };
 
 // Get All Oncall Events
+// hardcoded for 15 days prior and 15 days after
 exports.getAllOnCallEvents = async (access_token, schedule_id) => {
   var resp = "";
   try {
     resp = await axios.get(
-      `${GET_ALL_ONCALL_EVENTS}/${schedule_id}/events?currentDate=""&daysBefore=7&daysAfter=7`,
+      `${GET_ALL_ONCALL_EVENTS}/${schedule_id}/events?currentDate=""&daysBefore=15&daysAfter=15`,
       {
         headers: {
           Authorization: `Bearer ${access_token} `,
