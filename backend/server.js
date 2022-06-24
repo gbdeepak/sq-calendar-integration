17b7bd6095bc2124f2c8263b5fd03f6cb890ac3e
Deepak Balakrishna <deepakbalakrishna@Deepaks-MacBook-Pro.local>
2022-06-24 11:06:01 -0700

    Changed welcome message

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,17 @@
-
 // server.js
-const https = require('https');
-const express = require('express');
-const fs = require('fs');
+const https = require("https");
+const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.use(express.static('static'));
-express.static.mime.define({'text/calendar': ['ics']});
+app.use(express.static("static"));
+express.static.mime.define({ "text/calendar": ["ics"] });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-// https.createServer({
-//     key: fs.readFileSync('server.key'),
-//     cert: fs.readFileSync('server.cert')
-//   }, app).listen(port, () => {
-//     console.log(`listening on port ${port}`)
-//   });
-
-
+app.get("/", (req, res) => {
+  res.send("Welcome to Squadcast Hackathon 2022");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
